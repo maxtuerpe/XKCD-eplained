a2b9cf6a97ddd953746eb80073fdc9e661a75fd2
Max Tuerpe <maxtuerpe4@gmail.com>
2018-11-29 14:06:25 -0700

    can you please work now?

--- express-CRUD/index.js
+++ express-CRUD/index.js
@@ -1,22 +1,24 @@
 const express        = require('express');
 const app            = express();
+const path           = require('path')
 const bodyParser     = require('body-parser');
 const cors           = require('cors');
 const morgan         = require('morgan');
 const session        = require('express-session')
 
 require('./db/db');
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(session({
-  secret: 'AHHHHHHHHHHHHHHHHidontknowwhatimdoing',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }))
 const corsOptions = {
-  origin: 'https://xkcd-explained.herokuapp.com/',
+  origin: process.env.REACT_APP_ADDRESS,
   credentials: true,
   optionsSuccessStatus: 200 
 }
-app.use(cors(corsOptions));
+app.use(cors({corsOptions}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -28,7 +30,13 @@ app.use(morgan('short'));
 const contributionController = require('./controllers/contributions');
 
 
-app.use('/contributions', contributionController);
+
+app.use('/api/v1/contributions', contributionController);
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
 
 
 app.listen(process.env.PORT || 9000, () => {
